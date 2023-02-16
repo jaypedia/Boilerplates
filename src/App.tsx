84a5273711b6ffecdcfbae1bf5a5ef5f8bb989d6
Millie <jay.lee.2280@gmail.com>
2023-02-16 12:10:32 +0900

    Move router to App.tsx & add RecoilRoot

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,26 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
-import { RouterProvider } from 'react-router-dom';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import { RecoilRoot } from 'recoil';
 
-import { router } from '@/router';
+import { Root, Error } from '@/pages';
 
 const queryClient = new QueryClient();
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Root />,
+    errorElement: <Error />,
+  },
+]);
 
 export const App = () => {
   return (
     <QueryClientProvider client={queryClient}>
-      <RouterProvider router={router} />
+      <RecoilRoot>
+        <RouterProvider router={router} />
+      </RecoilRoot>
       <ReactQueryDevtools />
     </QueryClientProvider>
   );
